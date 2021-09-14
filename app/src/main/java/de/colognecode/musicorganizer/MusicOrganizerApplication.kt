8c862914f09c64897e173332230935205f310809
Dimitri Simon <dimitri_simon@posteo.de>
2021-09-14 20:22:13 +0200

    - added hilt as dependency injection to the project

---
+++ app/src/main/java/de/colognecode/musicorganizer/MusicOrganizerApplication.kt
@@ -0,0 +1,7 @@
+package de.colognecode.musicorganizer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MusicOrganizerApplication : Application()
